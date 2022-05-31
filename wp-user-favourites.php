1e100afb760090b0e012b19cdda699b38162db7c
Mads Paaskesen <madspaaskesen@gmail.com>
2022-05-31 13:01:27 +0200

    WILL-3634/Search-results---all-Willow-sites
    follow right version numbers in our files.

--- wp-user-favourites.php
+++ wp-user-favourites.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: User Favourites
- * Version: 1.1.4
+ * Version: 2.0.1
  * Plugin URI: https://github.com/BenjaminMedia/wp-user-favourites
  * Description: A plugin for marking articles as favourites.
  * Author: Bonnier Interactive
